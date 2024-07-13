85ab04aedce441895028eacef4bb4268152e0682
gs97ahn <gs97ahn@naver.com>
2024-07-14 01:56:38 +0900

    [FIX] 닉네임 생성 로직 디버깅

--- src/main/kotlin/com/woomulwoomul/woomulwoomulbackend/common/utils/UserUtils.kt
+++ src/main/kotlin/com/woomulwoomul/woomulwoomulbackend/common/utils/UserUtils.kt
@@ -10,10 +10,17 @@ class UserUtils {
         private val threadLocalRandom = ThreadLocalRandom.current()
 
         fun generateRandomNickname(nickname: String): String {
+            println("generateRandomNickname")
+            println("1|".plus(nickname.substring(0, 6)))
+            println("2|".plus(nickname.substring(0, 6)).plus("_").plus(threadLocalRandom.nextInt(1, 1000)))
+
             return nickname.substring(0, 6).plus("_").plus(threadLocalRandom.nextInt(1, 1000))
         }
 
         fun getNicknameFromEmail(email: String): String {
+            println("getNicknameFromEmail")
+            println("1|".plus(email.split("@")[0]))
+            println("2|".plus(email.split("@")[0].substring(0, 10)))
             return email.split("@")[0].substring(0, 10)
         }
 
