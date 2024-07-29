dbd1531700326f46ddf98a86a2d7bafb659210b3
gs97ahn <gs97ahn@naver.com>
2024-07-29 23:16:24 +0900

    [CHORE] change docker image tag name

---
+++ src/test/kotlin/com/woomulwoomul/woomulwoomulbackend/domain/user/GenderTest.kt
@@ -0,0 +1,20 @@
+package com.woomulwoomul.woomulwoomulbackend.domain.user
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class GenderTest {
+
+    @Test
+    fun getEntries() {
+    }
+
+    @Test
+    fun values() {
+    }
+
+    @Test
+    fun valueOf() {
+    }
+}
---
+++ src/test/kotlin/com/woomulwoomul/woomulwoomulbackend/domain/user/RoleTest.kt
@@ -0,0 +1,24 @@
+package com.woomulwoomul.woomulwoomulbackend.domain.user
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class RoleTest {
+
+    @Test
+    fun getText() {
+    }
+
+    @Test
+    fun getEntries() {
+    }
+
+    @Test
+    fun values() {
+    }
+
+    @Test
+    fun valueOf() {
+    }
+}
