4f6986e9e98179c6ced012eb5232751f4449a807
gs97ahn <gs97ahn@naver.com>
2024-08-05 22:33:50 +0900

    [REFACTOR] OAuth login url path 변경

---
+++ src/test/kotlin/com/woomulwoomul/woomulwoomulbackend/domain/base/DetailServiceStatusTest.kt
@@ -0,0 +1,24 @@
+package com.woomulwoomul.woomulwoomulbackend.domain.base
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class DetailServiceStatusTest {
+
+    @Test
+    fun getText() {
+    }
+
+    @Test
+    fun getEntries() {
+    }
+
+    @Test
+    fun values() {
+    }
+
+    @Test
+    fun valueOf() {
+    }
+}
