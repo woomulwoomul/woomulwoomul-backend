fcc796a17e3e3c07cd768b1a77b32eac76886706
gs97ahn <gs97ahn@naver.com>
2024-09-27 23:44:37 +0900

    [CHORE] redis config 삭제

--- client-api/src/main/kotlin/com/woomulwoomul/clientapi/config/RedisConfig.kt
+++ client-api/src/main/kotlin/com/woomulwoomul/clientapi/config/RedisConfig.kt
@@ -1,25 +1,25 @@
-package com.woomulwoomul.clientapi.config
-
-import org.redisson.Redisson
-import org.redisson.api.RedissonClient
-import org.redisson.config.Config
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
-
-@Profile("!local & !test")
-@Configuration
-class RedisConfig(
-    private val redisProperties: RedisProperties,
-) {
-
-    @Bean
-    fun redissonClient(): RedissonClient {
-        val config = Config()
-        config.useSingleServer()
-            .setAddress("redis://" + redisProperties.host + ":" + redisProperties.port)
-            .setPassword(redisProperties.password)
-        return Redisson.create(config)
-    }
-}
+//package com.woomulwoomul.clientapi.config
+//
+//import org.redisson.Redisson
+//import org.redisson.api.RedissonClient
+//import org.redisson.config.Config
+//import org.springframework.boot.autoconfigure.data.redis.RedisProperties
+//import org.springframework.context.annotation.Bean
+//import org.springframework.context.annotation.Configuration
+//import org.springframework.context.annotation.Profile
+//
+//@Profile("!local & !test")
+//@Configuration
+//class RedisConfig(
+//    private val redisProperties: RedisProperties,
+//) {
+//
+//    @Bean
+//    fun redissonClient(): RedissonClient {
+//        val config = Config()
+//        config.useSingleServer()
+//            .setAddress("redis://" + redisProperties.host + ":" + redisProperties.port)
+//            .setPassword(redisProperties.password)
+//        return Redisson.create(config)
+//    }
+//}
